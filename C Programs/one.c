ba678571025f6b3b92734f1f74b350d7c10ad863
Harini <harinisree333@gmail.com>
2024-06-13 20:04:14 +0530

    the aptitude problem are started to solve from today

---
+++ C Programs/one.c
@@ -0,0 +1,14 @@
+//1)
+// #include <studio.h>
+
+// int main()
+// {
+//     for(int n=7;n!=0;n--){
+//         printf("n=%d",n--);
+//     }
+//     getchar();
+//     return 0;
+// }
+
+//it is like an infinite loop.
+//op : 7 5 3 1 -1 .......
