5af4147c808f36fe62e6690e1de58d59199b9cef
Harini <harinisree333@gmail.com>
2024-06-13 20:16:09 +0530

    The c questions are updated

--- C Programs/one.c
+++ C Programs/one.c
@@ -12,3 +12,30 @@
 
 //it is like an infinite loop.
 //op : 7 5 3 1 -1 .......
+
+//2)
+// int main()
+// {
+//     int i=1;
+//     do{
+//         printf("%d\n",i++);
+//         if(i<15)
+//         break;
+//     }while(1);
+//     return 0;
+// }
+
+//op :1
+
+// int main()
+// {
+//     int i=1;
+//     do{
+//         printf("%d\n",i++);
+//         if(i<15)
+//         continue;
+//     }while(0);
+//     return 0;
+// }
+
+//op : 1
