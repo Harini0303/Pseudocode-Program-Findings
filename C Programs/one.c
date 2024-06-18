67e96681b78b97568cc7bec98f64bd2218a5a9e9
Harini <harinisree333@gmail.com>
2024-06-18 15:49:14 +0530

    the problems are updated

--- C Programs/one.c
+++ C Programs/one.c
@@ -1,6 +1,15 @@
-//1)
-// #include <studio.h>
 
+#include <studio.h>
+
+// int main()
+// {
+//     printf("Hello World");
+
+//     return 0;
+// }
+
+
+//1)
 // int main()
 // {
 //     for(int n=7;n!=0;n--){
@@ -39,3 +48,51 @@
 // }
 
 //op : 1
+
+//3)
+// #include <stdio.h>
+
+// char* getString(){
+//     char* str ="Nice test for strings";
+//     return str;
+// }
+
+// int main()
+// {
+//     printf("%s",getString());
+
+//     return 0;
+// }
+
+//op : Nice test for strings
+
+// char getString(){
+//     char str ="Nice test for strings";
+//     return str;
+// }
+
+// int main()
+// {
+//     printf("%s",getString());
+
+//     return 0;
+// }
+
+//op :  error
+
+//static variable in c : A static int  variable remains in memory while the programm is running.
+//  A normal or auto variable is destroyed when a 
+// function call where the variable was declared is over.
+
+
+//4)
+// int main(){
+//     static int i=5;
+//     if(--i){
+//         main(); //recursion
+//         printf("%d",i);
+//     }
+// }
+
+//op : 4 3 2 1 0
+//when it become 0 the recursion base condition fails and return and print the values.
