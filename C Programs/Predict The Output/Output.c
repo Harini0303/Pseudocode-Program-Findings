da625c0427647b9109cf10eefd443437c1ce3945
Harini <harinisree333@gmail.com>
2024-08-04 16:34:42 +0530

    Updated

--- C Programs/Predict The Output/Output.c
+++ C Programs/Predict The Output/Output.c
@@ -89,6 +89,88 @@ main()
 // case labels must be constants , not variable
 
 
+//6.
+main()
+{
+    if('A' < 'a'){
+        printf("TCS");
+    }else{
+        printf("CTS");
+    }
+}
+
+// op : TCS 
+
+
+// 7.
+#define TEST 5
+main()
+{
+    printf("TEST"); // if  printf("%d",TEST) then 5
+}
+
+// op : TEST
+// macro written within double quotes will be treated as strings only
+
+
+// 8.
+main()
+{
+    int a = 3;
+
+    switch(a){
+        case 1: 
+        case 5: 
+        case 6: printf("America");
+                  break;
+        case 2: printf("India");
+                  break;
+        case 3:
+        case 7: printf("Australia");
+                break;
+        default : printf("To the moon");
+    }
+}
+
+// op : Australia
+// we can take vacant cases in switch cases
+
+//9.
+main()
+{
+    switch(printf("Moon")){ // no of char in the printf function that checks and return the value - 4
+        case 1: printf("Case 1");
+                  break;
+        case 2: printf("Case 2");
+                  break;
+        case 3: printf("Case 3");
+                  break;
+        case 4: printf("Case 4");
+                  break;
+        default : printf("Bye");
+    }
+}
+
+// op : MoonCase 4
+// printf () function return an integer value
+
+// 10.
+main()
+{
+    int n =4;
+    switch(n){ 
+        case 1: printf("Case 1");
+                  break;
+        case 2: printf("Case 2");
+                  break;
+        case 3: printf("Case 3");
+                  break;
+    }
+}
+
+// op : empty output screen with sucessful compilation
+// op : default block is completely optional
+
 
 
 
